4e3da38d44795b727afefc13724a692c48b9ef23
DAgutierrez <diego.gutierrez684@gmail.com>
2016-05-19 17:07:22 -0300

    first commit

---
+++ app/src/test/java/diego_gutierrez/android_user_interface/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package diego_gutierrez.android_user_interface;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
